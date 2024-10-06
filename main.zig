ebe4939d75795755ef5a2b3e5013a0730748e9e0
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-10-06 17:16:31 +0100

    basic window creation

--- main.zig
+++ main.zig
@@ -2,35 +2,48 @@ const std = @import("std");
 const zlm = @import("zlm");
 
 const c = @cImport({
-    @cDefine("false", "0");
     @cDefine("GLFW_INCLUDE_VULKAN", {});
     @cInclude("GLFW/glfw3.h");
 });
 
+const WIDTH = 800;
+const HEIGHT = 600;
+
 pub fn main() void {
-    _ = c.glfwInit();
-    c.glfwWindowHint(c.GLFW_CLIENT_API, c.GLFW_NO_API);
-    const windowPtr = c.glfwCreateWindow(800, 600, "Vulkan Window", null, null);
+    const window = initWindow();
+    initVulkan();
 
-    var extension_count: u32 = undefined;
-    _ = c.vkEnumerateInstanceExtensionProperties(null, &extension_count, null);
-    std.debug.print("Extensions supported: {}\n", .{extension_count});
-
-    const mat = zlm.Mat4{ .fields = [4][4]f32{
-        [4]f32{ 1, 0, 0, 0 },
-        [4]f32{ 0, 2, 0, 0 },
-        [4]f32{ 0, 0, 3, 0 },
-        [4]f32{ 0, 0, 0, 4 },
-    } };
-    const vec = zlm.Vec4.new(1, 2, 3, 4);
-
-    const product = zlm.Vec4.transform(vec, mat);
-    std.debug.print("Product: {any}\n", .{product});
-
-    while (c.glfwWindowShouldClose(windowPtr) == c.false) {
+    // mainLoop
+    while (c.glfwWindowShouldClose(window) == c.GLFW_FALSE) {
         c.glfwPollEvents();
     }
-
-    c.glfwDestroyWindow(windowPtr);
+    // NOTE: worth checking what is gained by calling this, guessing resources will get freed by OS on program exit anyway?
+    // cleanup
+    c.glfwDestroyWindow(window);
     c.glfwTerminate();
+}
+
+fn initWindow() *c.GLFWwindow {
+    const init_res = c.glfwInit();
+    if (init_res == c.GLFW_FALSE) {
+        std.debug.print("init_res: {}\n", .{init_res});
+        fatal("GLFW init failed");
+    }
+
+    // GLFW originally intended to create OpenGL context, this tells it not to
+    c.glfwWindowHint(c.GLFW_CLIENT_API, c.GLFW_NO_API);
+    c.glfwWindowHint(c.GLFW_RESIZABLE, c.GLFW_FALSE);
+
+    return c.glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", null, null) orelse fatal("GLFW window creation failed");
+}
+
+fn initVulkan() void {}
+
+fn fatal(comptime mess: []const u8) noreturn {
+    std.debug.print(mess, .{});
+    std.process.exit(1);
+}
+
+fn fatalQuiet() noreturn {
+    fatal("");
 }
